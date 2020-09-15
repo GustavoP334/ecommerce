8945aed1866d067ddfbdfd044ae5096a5e345259
Gustavo <gpassosdelima@gmail.com>
2020-09-14 21:13:43 -0300

    Adicionando cadastro, alteração e exluindo usuários

---
+++ views/admin/htdocs/AutoLoad/Automovel.php
@@ -0,0 +1,35 @@
+<?php
+
+interface Veiculo {
+
+	public function acelerar($velocidade);
+	public function frenar($velocidade);
+	public function trocarMarcha($marcha);
+
+}
+
+abstract class Automovel implements Veiculo {
+
+	public function acelerar($velocidade)
+	{
+
+		echo "O veículo acelerou até " . $velocidade . "KM/h";
+
+	}
+
+	public function frenar($velocidade)
+	{
+
+		echo "O veículo frenou até " . $velocidade . "KM/h";
+
+	}
+
+	public function trocarMarcha($marcha)
+	{
+
+		echo "o veículo engatou a " . $marcha . "ª marcha.";
+
+	}
+}
+
+?>
